6f89baf1fa8035ce1188281fc399d2ee82151ad8
Takafumi Arakaki <aka.tkf@gmail.com>
2011-05-22 01:19:18 +0200

    changed setup.py to include neorg/static/help for package.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+from os import path
 import neorg
+
+data_list = ['schema.sql', 'templates/*.html'] + [
+    path.join('%s' % d, '*.%s' % e)
+    for e in ['css', 'html', 'ico', 'inv', 'js', 'png', 'txt']
+    for d in ['static', 'static/help',
+              'static/help/_source', 'static/help/_static',]]
 
 setup(
     name='neorg',
     version=neorg.__version__,
     packages=['neorg'],
+    package_data={
+        'neorg': data_list},
     description='NEOrg - Numerical Simulation Organizer',
     long_description=neorg.__doc__,
     author=neorg.__author__,
