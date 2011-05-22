1fa65a9152e61c21475a2736ac9fa82f0fb3a0d4
Takafumi Arakaki <aka.tkf@gmail.com>
2011-05-22 17:48:49 +0200

    made update_help command which runs before the build_py.

--- setup.py
+++ setup.py
@@ -1,12 +1,54 @@
 from setuptools import setup
-from os import path
+from distutils.command.build import build
+from distutils.cmd import Command
+import os
+import shutil
 import neorg
 
 data_list = ['schema.sql', 'templates/*.html'] + [
-    path.join('%s' % d, '*.%s' % e)
+    os.path.join('%s' % d, '*.%s' % e)
     for e in ['css', 'html', 'ico', 'inv', 'js', 'png', 'txt']
     for d in ['static', 'static/help',
               'static/help/_source', 'static/help/_static',]]
+
+
+class my_build(build):
+    sub_commands = [  # run update_help before any other sub_commands
+        ('update_help', lambda _: True),
+        ] + build.sub_commands
+
+
+class update_help(Command):
+    description = 'Copy document to neorg/static/help'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # run build_sphinx before copying
+        for cmd_name in self.get_sub_commands():
+            self.run_command(cmd_name)
+
+        sphinx_build_dir = 'doc/build/html'
+        static_help = 'neorg/static/help'
+        if os.path.exists(static_help):
+            shutil.rmtree(static_help)
+        print "copying '%s' to '%s'" % (sphinx_build_dir, static_help)
+        shutil.copytree(sphinx_build_dir, static_help)
+
+    sub_commands = [
+        ('build_sphinx', lambda _: True),
+        ]
+
+
+# build directory is needed to be created before running build_sphinx
+if not os.path.exists("doc/build"):
+    os.mkdir("doc/build")
+
 
 setup(
     name='neorg',
@@ -30,5 +72,15 @@ setup(
         "Topic :: Scientific/Engineering",
         ],
     entry_points = {
-        'console_scripts': ['neorg = neorg.commands:main']}
+        'console_scripts': ['neorg = neorg.commands:main']},
+    command_options={
+        'build_sphinx': {  # specify the help document
+            'source_dir': ('setup.py', 'doc/source/'),
+            'build_dir': ('setup.py', 'doc/build/'),
+            },
+        },
+    cmdclass={
+        'build': my_build,
+        'update_help': update_help,
+        },
     )
