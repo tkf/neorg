3b4ca2c1ad65423631a2c663be6aa320c4e3af3a
Takafumi Arakaki <aka.tkf@gmail.com>
2011-05-22 23:49:33 +0200

    added `TestNEOrgWeb.test_delete_{yes,no}`.

--- neorg/tests/test_web.py
+++ neorg/tests/test_web.py
@@ -21,6 +21,25 @@ from neorg.config import DefaultConfig
 TMP_PREFIX = 'neorg-tmp'
 
 
+def urljoin(a, *p):
+   """
+   Join two or more pathname components, inserting '/' as needed.
+
+   If any component is an absolute path, all previous path components
+   will be discarded. (Adapted from the `posixpath` module.)
+
+   """
+   path = a
+   for b in p:
+       if b.startswith('/'):
+           path = b
+       elif path == '' or path.endswith('/'):
+           path +=  b
+       else:
+           path += '/' + b
+   return path
+
+
 def setup_app():
     web.app.config.from_object(DefaultConfig)
     (db_fd, web.app.config['DATABASE']) = tempfile.mkstemp(prefix=TMP_PREFIX)
@@ -69,6 +88,26 @@ else:
 
 
 class TestNEOrgWeb(TestNEOrgWebBase):
+    num_test = 3  # Number of test for generated page
+
+    @staticmethod
+    def gene_page_paths(num, prefix="test"):
+        yield ""
+        page_path = prefix
+        for i in range(1, num):
+            yield page_path
+            page_path += "/%s-%d" % (prefix, i)
+
+    @staticmethod
+    def gene_page_texts(num, prefix="test"):
+        yield "this is root page for prefix=%s" % prefix
+        for i in range(1, num):
+            yield "this is %d-th page for prefix=%s" % (i, prefix)
+
+    def gene_pages(self, prefix="test", num=None):
+        num = self.num_test if num is None else num
+        return zip(self.gene_page_paths(num, prefix),
+                   self.gene_page_texts(num, prefix))
 
     def test_empty_db(self):
         response = self.app.get('/')
@@ -80,13 +119,13 @@ class TestNEOrgWeb(TestNEOrgWebBase):
     def check_save(self, page_path, page_text, page_html=None):
         if page_html is None:
             page_html = self.gene_html(page_text)
-        save_path = ("/_save" if page_path == "" else
-                     "/".join([page_path, "_save"]))
+        save_path = urljoin(page_path, "_save")
         response = self.app.post(save_path, data={
             'save': 'Save',
             'page_text': page_text,
             }, follow_redirects=True)
         assert page_html in response.data
+        return (response, page_html)
 
     @raises(AssertionError)
     def fail_check_save(self, *args, **kwds):
@@ -94,11 +133,34 @@ class TestNEOrgWeb(TestNEOrgWebBase):
     # fail_check_save = raises(AssertionError)(check_save)  # does not work!
 
     def test_save(self):
-        data = [("", "This is my first post"),
-                ("", "This is my second post"),
-                ("SubPage", "This is my subpage")]
         fake_html = "But this is not!"
-
-        for (page_path, page_text) in data:
+        for (page_path, page_text) in self.gene_pages("TestSave"):
             yield (self.check_save, page_path, page_text)
             yield (self.fail_check_save, page_path, page_text, fake_html)
+
+    def check_delete_yes(self, page_path, page_text):
+        (save_response, page_html,
+         ) = self.check_save(page_path, page_text)
+        delete_path = urljoin(page_path, "_delete")
+        response = self.app.post(delete_path, data={
+            'yes': 'Yes',
+            }, follow_redirects=True)
+        assert page_html not in response.data
+        assert '<form action="/_save"' in response.data
+
+    def check_delete_no(self, page_path, page_text):
+        (save_response, page_html,
+         ) = self.check_save(page_path, page_text)
+        delete_path = urljoin(page_path, "_delete")
+        response = self.app.post(delete_path, data={
+            'no': 'No',
+            }, follow_redirects=True)
+        assert page_html in response.data
+
+    def test_delete_yes(self):
+        for (page_path, page_text) in self.gene_pages("TestDelYes"):
+            yield (self.check_delete_yes, page_path, page_text)
+
+    def test_delete_no(self):
+        for (page_path, page_text) in self.gene_pages("TestDelNo"):
+            yield (self.check_delete_no, page_path, page_text)
