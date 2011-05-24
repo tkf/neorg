bbc2b1201efaae90d7ef09850e05b86e6fd807d6
Takafumi Arakaki <aka.tkf@gmail.com>
2011-05-24 09:38:25 +0200

    added tests for all url defined by `@app.route`

--- neorg/tests/test_web.py
+++ neorg/tests/test_web.py
@@ -11,6 +11,7 @@ setup/teardown.
 """
 
 import os
+import re
 import tempfile
 import shutil
 from nose.tools import raises
@@ -109,17 +110,10 @@ class TestNEOrgWeb(TestNEOrgWebBase):
         return zip(self.gene_page_paths(num, prefix),
                    self.gene_page_texts(num, prefix))
 
-    def test_empty_db(self):
-        response = self.app.get('/')
-        assert response.location == "http://localhost/_edit"
-
-        response = self.app.get('/', follow_redirects=True)
-        assert '<form action="/_save"' in response.data
-
     def check_save(self, page_path, page_text, page_html=None):
         if page_html is None:
             page_html = self.gene_html(page_text)
-        save_path = urljoin(page_path, "_save")
+        save_path = urljoin('/', page_path, "_save")
         response = self.app.post(save_path, data={
             'save': 'Save',
             'page_text': page_text,
@@ -141,7 +135,7 @@ class TestNEOrgWeb(TestNEOrgWebBase):
     def check_delete_yes(self, page_path, page_text):
         (save_response, page_html,
          ) = self.check_save(page_path, page_text)
-        delete_path = urljoin(page_path, "_delete")
+        delete_path = urljoin('/', page_path, "_delete")
         response = self.app.post(delete_path, data={
             'yes': 'Yes',
             }, follow_redirects=True)
@@ -151,7 +145,7 @@ class TestNEOrgWeb(TestNEOrgWebBase):
     def check_delete_no(self, page_path, page_text):
         (save_response, page_html,
          ) = self.check_save(page_path, page_text)
-        delete_path = urljoin(page_path, "_delete")
+        delete_path = urljoin('/', page_path, "_delete")
         response = self.app.post(delete_path, data={
             'no': 'No',
             }, follow_redirects=True)
@@ -164,3 +158,127 @@ class TestNEOrgWeb(TestNEOrgWebBase):
     def test_delete_no(self):
         for (page_path, page_text) in self.gene_pages("TestDelNo"):
             yield (self.check_delete_no, page_path, page_text)
+
+    def check_confirm_delete(self, page_path, page_text):
+        (save_response, page_html,
+         ) = self.check_save(page_path, page_text)
+        confirm_delete_path = urljoin('/', page_path, "_confirm_delete")
+        response = self.app.get(confirm_delete_path)
+        assert page_html in response.data
+        delete_path = urljoin('/', page_path, "_delete")
+        assert '<form action="%s"' % delete_path in response.data
+        assert '<input type="submit" name="yes" value="Yes" />' in response.data
+        assert '<input type="submit" name="no" value="No" />' in response.data
+
+    def test_confirm_delete(self):
+        for (page_path, page_text) in self.gene_pages("TestConfirmDel"):
+            yield (self.check_confirm_delete, page_path, page_text)
+
+    def check_edit_new_page(self, page_path, page_text):
+        (save_response, page_html,
+         ) = self.check_save(page_path, page_text)
+        edit_path = urljoin('/', page_path, "_edit")
+        response = self.app.get(edit_path, follow_redirects=True)
+        assert page_text in response.data
+        return (response, page_html)
+
+    def check_edit_existing_page(self, page_path, page_text):
+        (response, page_html,
+         ) = self.check_edit_new_page(page_path, page_text)
+        assert page_html in response.data
+
+    def test_edit_new_page(self):
+        for (page_path, page_text) in self.gene_pages("TestEditNew"):
+            if page_path == "":
+                continue
+            yield (self.check_edit_new_page, page_path, page_text)
+
+    def test_edit_existing_page(self):
+        for (page_path, page_text) in self.gene_pages("TestEditExisting"):
+            yield (self.check_edit_existing_page, page_path, page_text)
+
+    def check_preview(self, page_path, page_text, page_html=None):
+        if page_html is None:
+            page_html = self.gene_html(page_text)
+        preview_path = urljoin('/', page_path, "_save")
+        response = self.app.post(preview_path, data={
+            'preview': 'Preview',
+            'page_text': page_text,
+            }, follow_redirects=True)
+        assert page_html in response.data
+        assert page_text in response.data
+        assert '<form action="%s"' % preview_path in response.data
+
+    def test_preview(self):
+        for (page_path, page_text) in self.gene_pages("TestPreview"):
+            yield (self.check_preview, page_path, page_text)
+
+    def test_descendants(self):
+        page_root = 'TestDescendantsRoot'
+        list_relpath = []
+        for (page_relpath, page_text) in self.gene_pages("TestDescendants"):
+            list_relpath.append(page_relpath)
+            page_path = urljoin(page_root, page_relpath)
+            self.check_save(page_path, page_text)
+
+        desc_path = urljoin('/', page_path, "_descendants")
+        response = self.app.get(desc_path, follow_redirects=True)
+        for page_relpath in list_relpath:
+            assert page_relpath in response.data
+
+    def check_history(self, page_path, page_text, num_update=5):
+        page_text_history = []
+        for dummy in range(num_update):
+            page_text_history.append(page_text)
+            self.check_save(page_path, page_text)
+            page_text = '"%s" is older than this' % page_text
+        hist_path = urljoin('/', page_path, "_history")
+        response = self.app.get(hist_path)
+        old_links = re.findall('<a href=".*_old/[0-9]*">', response.data)
+        assert num_update == len(old_links)
+        assert 1 == len(re.findall('\n *Newest\n', response.data))
+        return (response, page_text_history)
+
+    def test_history(self):
+        page_root = 'TestHistRoot'
+        for (page_relpath, page_text) in self.gene_pages("TestHist"):
+            page_path = urljoin(page_root, page_relpath)
+            yield (self.check_history, page_path, page_text)
+
+    def check_old(self, page_path, page_text, num_update=5):
+        (hist_response, page_text_history,
+         )= self.check_history(page_path, page_text, num_update)
+        old_links = re.findall('<a href="(.*_old/[0-9]*)">',
+                               hist_response.data)
+        assert len(page_text_history) == len(old_links)
+        for (link, old_text) in zip(old_links,
+                                    reversed(page_text_history)):
+            response = self.app.get(link)
+            old_html = self.gene_html(old_text)
+            assert old_html in response.data
+
+    def test_old(self):
+        page_root = 'TestOldRoot'
+        for (page_relpath, page_text) in self.gene_pages("TestOld"):
+            page_path = urljoin(page_root, page_relpath)
+            yield (self.check_old, page_path, page_text)
+
+    def test_help(self):
+        response = self.app.get('/_help/index.html')
+        assert response.location == "http://localhost/static/help/index.html"
+
+        response = self.app.get('/_help/index.html',
+                                follow_redirects=True)
+        import neorg
+        title = 'NEOrg v%s documentation' % neorg.__version__
+        assert title in response.data
+
+
+class TestNEOrgWebWithEmptyDB(TestNEOrgWebSlow):
+
+    def test_empty_db(self):
+        response = self.app.get('/')
+        assert response.location == "http://localhost/_edit"
+
+        response = self.app.get('/', follow_redirects=True)
+        assert '<form action="/_save"' in response.data
