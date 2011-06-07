8069ecc89d289ebd870062fc106a6d5e925867c3
Takafumi Arakaki <aka.tkf@gmail.com>
2011-06-08 01:19:15 +0200

    added `tools/neorg-ci.sh` to use it in jenkins-ci.

---
+++ tools/neorg-ci.sh
@@ -0,0 +1,10 @@
+python setup.py build
+nosetests --with-xunit
+touch nosetests.xml --date="$(date)"
+
+NEORG_FASTTEST=True nosetests \
+    --with-xunit --xunit-file=nosetests-fast-test.xml \
+    neorg/tests/test_web.py
+touch nosetests-fast-test.xml --date="$(date)"
+
+# note: `touch` is a workaround for incorrect NFS system time setting
