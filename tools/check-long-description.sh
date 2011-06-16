709d0bbff5cd9299110e1c86117e78b9062cd89f
Takafumi Arakaki <aka.tkf@gmail.com>
2011-06-16 16:21:49 +0200

    added tools for uploading to pypi: upload-to-pypi.sh and check-long-description.sh

---
+++ tools/check-long-description.sh
@@ -0,0 +1,16 @@
+# checking long description which is embedded in __init__.py
+
+htmlpath=.long-description.html
+
+if [ -d doc/ -a -d neorg/ ]
+then
+    (cd neorg && cog.py -r __init__.py) && \
+        python setup.py --long-description | rst2html > $htmlpath
+    gnome-open $htmlpath
+    echo "hg status"
+    hg status  # maybe cog.py updated  __init__.py
+else
+    echo "Execute $0 at root of the working directory."
+    echo 'You should see `doc/` and `nerog/` directories.'
+    exit 1
+fi
